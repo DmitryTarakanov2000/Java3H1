72fa7e44f1865b18eb4296bc6e9d624ba64ef96d
DmitryTarakanov2000 <77406313+DmitryTarakanov2000@users.noreply.github.com>
2021-03-19 15:55:05 +0300

    Add files via upload

---
+++ Task3/src/main/java/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        Box<Orange> or = new Box<>();
+        Box<Orange> or1 = new Box<>();
+        Box<Apple> ap = new Box<>();
+        Box<Apple> ap1 = new Box<>();
+        System.out.println("addFruit: ");
+        or.addFruit(new Orange(),10);
+        or1.addFruit(new Orange(),12);
+        ap.addFruit(new Apple(),8);
+        ap1.addFruit(new Apple(),4);
+        System.out.println("Box 1: "+or.getWeight());
+        System.out.println("Box 2: "+or1.getWeight());
+        System.out.println("Box 3: "+ap.getWeight());
+        System.out.println("Box 4: "+ap1.getWeight());
+        System.out.println("compare(): ");
+        System.out.println("Box 1 equals box 3: "+or.compare(ap));
+        System.out.println("Box 2 equals box 4: "+or1.compare(ap1));
+        System.out.println("  pourTo(): ");
+        or.pourTo(or1);
+        ap.pourTo(ap1);
+        System.out.println("getWeight(): ");
+        System.out.println("Box 1: "+or.getWeight());
+        System.out.println("Box 2: "+or1.getWeight());
+        System.out.println("Box 3: "+ap.getWeight());
+        System.out.println("Box 4: "+ap1.getWeight());
+
+
+
+    }
+}
